70ec93bd870fc33019f48f16032456383bb56631
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-12-31 10:37:54 +0200

    Refactor getComputerChoice()

--- rps.js
+++ rps.js
@@ -2,9 +2,14 @@ function getRandomInt(max)
 { // returns a random integer from [0, max-1]
   return Math.floor(Math.random() * max);
 }
+function capitalizeFirstLetter(string)
+{
+  let c = string.charAt(0);
+  return c.toUpperCase() + string.slice(1);
+}
+const CHOICES = ["rock", "paper", "scissors"];
 function getComputerChoice()
 { // returns "Rock", "Paper" or "Scissors"
-  choices = ["Rock", "Paper", "Scissors"];
-  return choices[getRandomInt(choices.length)];
+  let choice = CHOICES[getRandomInt(CHOICES.length)];
+  return capitalizeFirstLetter(choice);
 }
-console.log(getComputerChoice());
